110a2445075e061a0008ea609afb0bfb8c760c1a
soyeon <soyeon20031010@gmail.com>
2024-12-19 21:58:28 +0900

    init: 프로젝트 초기 세팅

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import * as S from "../styles/Home/HomPageComponentStyle";
+const HomePage = () => {
+    return(
+        <>
+            <S.HomeDiv>test</S.HomeDiv>
+            <S.HomeDiv2>test</S.HomeDiv2>
+            <S.HomeDiv3>test</S.HomeDiv3>
+        </>
+    );
+};
+
+export default HomePage;
---
+++ src/styles/Home/HomPageComponentStyle.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const HomeDiv = styled.div`
+    color: var(--Main3);
+    font: var(--headingXL);
+`;
+
+export const HomeDiv2 = styled.div`
+    color: var(--Secondary2);
+    font: var(--labelXL);
+`;
+
+export const HomeDiv3 = styled.div`
+    color: var(--Main5);
+    font: var(--paragraphXL);
+`;
