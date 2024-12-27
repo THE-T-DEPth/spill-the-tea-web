11a62564faa9c2b3d16ffaea2c7dfdf014d6e18e
soyeon <soyeon20031010@gmail.com>
2024-12-27 16:38:50 +0900

    chore: 폰트 컬러 추가 및 prettier 설치 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import FindPassPage from "./pages/FindPassPage";
-import CertificationNumPage from "./pages/CertificationNumPage";
-import SearchResultPage from "./pages/SearchResultPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import FindPassPage from './pages/FindPassPage';
+import CertificationNumPage from './pages/CertificationNumPage';
+import SearchResultPage from './pages/SearchResultPage';
+import ViewDetailSsul from './pages/ViewDetailSsul';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/find-password" element={<FindPassPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/find-password' element={<FindPassPage />} />
         <Route
-          path="/certification-number"
+          path='/certification-number'
           element={<CertificationNumPage />}
         />
-        <Route path="/searchresult" element={<SearchResultPage />} />
+        <Route path='/searchresult' element={<SearchResultPage />} />
+        <Route path='/viewDetailSsul' element={<ViewDetailSsul />} />
       </Routes>
     </BrowserRouter>
   );
