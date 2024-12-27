6bd8ed80774a822f41518d11a44a62473ea053ba
hayoung Park <mcd158@naver.com>
2024-12-27 18:16:00 +0900

    feat : topBar 컴포넌트 구현 , 아이콘 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 import Header from "../components/layout/Header"
 import Footer from "../components/layout/Footer"
 import NavBar from "../components/layout/NavBar";
+import TopBar from "../components/Home/TopBar";
 import * as S from "../styles/Home/HomPageComponentStyle";
+
 const HomePage = () => {
 	return (
 		<>
 			<Header />
 			<NavBar />
+			<TopBar text='"오늘도 썰 한 잔, 짤 한 스푼 "' />
 			<S.HomeDiv>test</S.HomeDiv>
 			<S.HomeDiv2>test</S.HomeDiv2>
 			<S.HomeDiv3>test</S.HomeDiv3>
