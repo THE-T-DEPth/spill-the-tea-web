c010631b3d6f1c2f437f54a6fcdb264699f68440
soyeon <soyeon20031010@gmail.com>
2024-12-27 16:38:50 +0900

    chore: 폰트 컬러 추가 및 prettier 설치 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ViewDetailSsul from "./pages/ViewDetailSsul";
 
 const Router = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+    return(
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/viewDetailSsul" element={<ViewDetailSsul/>}/>
+        </Routes>
+      </BrowserRouter>
+    );
 }
 
 export default Router;
