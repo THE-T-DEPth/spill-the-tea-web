7bc94c952c1eca0544a22bd25a1d6b8506632c82
hayoung Park <mcd158@naver.com>
2024-12-30 01:09:42 +0900

    fix : 충돌 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,22 +3,22 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import FindPassPage from './pages/FindPassPage';
 import CertificationNumPage from './pages/CertificationNumPage';
-import SearchResultPage from './pages/SearchResultPage';
 import ViewDetailSsul from './pages/ViewDetailSsul';
+import SearchResultPage from './pages/SearchResultPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/viewDetailSsul' element={<ViewDetailSsul />} />
+        <Route path='/searchresult' element={<SearchResultPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/find-password' element={<FindPassPage />} />
         <Route
           path='/certification-number'
           element={<CertificationNumPage />}
         />
-        <Route path='/searchresult' element={<SearchResultPage />} />
-        <Route path='/viewDetailSsul' element={<ViewDetailSsul />} />
       </Routes>
     </BrowserRouter>
   );
