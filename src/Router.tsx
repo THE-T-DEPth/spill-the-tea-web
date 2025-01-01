fca09e2fc27ecbbe15cf32aa2d607d8e8de24f2a
hayoung Park <mcd158@naver.com>
2025-01-02 02:22:37 +0900

    feat : Router수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./components/layout/Layout";
 
+
 const Router = () => {
 	return (
 		<BrowserRouter>
