393fe008bec8667adef8ba0a5020f68ae25b217a
hayoung Park <mcd158@naver.com>
2025-01-03 11:44:45 +0900

    feat : 티만들기 버튼 컴포넌트 구현

--- src/styles/Home/MakeTeaButtonStyle.ts
+++ src/styles/Home/MakeTeaButtonStyle.ts
@@ -4,18 +4,25 @@ export const ButtonContainer = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color:var(--EarlGrey);
-  color:var(--Sub1);
+  background-color:var(--EarlGrey); /* 기본 색상 */
+  color: var(--Sub1);
   font: var(--headingLarge);
+  width: 213px;
+  height: 84px;
   padding: 12px 24px;
   border: none;
-  border-radius: 8px;
+  border-radius: 13px;
   cursor: pointer;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.2);
+  position: relative; 
+  overflow: visible; 
 
-  &:hover {
-    background-color: #5c5945;
-  }
+  /* Gradient Stroke */
+  border: 2px solid transparent;
+  background-image: linear-gradient(#55533e, #55533e), /* 기본 배경 */
+    linear-gradient(141.28deg, #86846a 0%, #312f20 100%); /* Stroke Gradient */
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
 
   &:active {
     background-color: #3f3d33;
@@ -23,7 +30,10 @@ export const ButtonContainer = styled.button`
 `;
 
 export const Leaf = styled.img`
-  width: 20px;
-  height: 20px;
-  margin-right: 8px;
+  width: 40px;
+  height: 35px;
+  position: absolute; 
+  top: 0px; 
+  left: 30px;
+  transform: translate(-50%, -50%); 
 `;
