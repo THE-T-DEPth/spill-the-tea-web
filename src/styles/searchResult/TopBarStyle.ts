d92bdc19820a777e61a11c090b2aedffec1fc32c
ksy132336 <ksygiru@gmail.com>
2025-01-04 20:27:00 +0900

    feat: TopBar background 수정

--- src/styles/searchResult/TopBarStyle.ts
+++ src/styles/searchResult/TopBarStyle.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 1440px;
   height: 42px;
-  background: var(--YellowBar);
+  background: var(--Sub2);
   display: flex;
   justify-content: center;
   align-items: center;
