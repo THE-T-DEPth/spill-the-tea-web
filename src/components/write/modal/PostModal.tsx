9848b9b90706dca67c8676789e4a4ca7fcabe0c5
soyeon <soyeon20031010@gmail.com>
2025-01-08 12:22:18 +0900

    feat: Writepage modal 구현

--- src/components/write/modal/PostModal.tsx
+++ src/components/write/modal/PostModal.tsx
@@ -3,13 +3,26 @@ import Close from '../../../assets/images/Close.svg';
 
 interface RemoveModalProps {
   setOpenPostModal: (value: boolean) => void;
+  titleInput: string | null;
+  textInput: any | undefined;
+  selectedThreeKeywords: string[];
+  imageCount: number;
 }
 
-// React.FC를 사용한 ComplainModal 정의
-const PostModal: React.FC<RemoveModalProps> = ({ setOpenPostModal }) => {
+const PostModal: React.FC<RemoveModalProps> = ({
+  setOpenPostModal,
+  titleInput,
+  textInput,
+  selectedThreeKeywords,
+  imageCount,
+}) => {
   const handleConfirmClick = () => {
     setOpenPostModal(false); // 모달 닫기
     window.location.href = '/';
+  };
+
+  const handleOkayClick = () => {
+    setOpenPostModal(false); // 모달 닫기
   };
 
   const handleViewPostClick = () => {
@@ -17,66 +30,53 @@ const PostModal: React.FC<RemoveModalProps> = ({ setOpenPostModal }) => {
     window.location.href = '/';
   };
 
+  const isTextEmpty =
+    !textInput ||
+    textInput.html.trim() === '' ||
+    textInput.html === '<p><br></p>';
+
+  // 조건별 메시지 설정
+  let modalMessage = '';
+  if (!titleInput || titleInput.trim() === '') {
+    modalMessage = '아차차,,\n제목을 정해주셔야 게시를 할 수 있어요!';
+  } else if (isTextEmpty) {
+    modalMessage = '아차차,,\n내용을 적어주셔야 게시를 할 수 있어요!';
+  } else if (imageCount == 0) {
+    modalMessage = '아차차,,\n짤을 최소 1개 지정해주셔야 게시를 할 수 있어요!';
+  } else if (selectedThreeKeywords.length !== 3) {
+    modalMessage = '아차차,,\n키워드를 지정해주셔야 게시를 할 수 있어요!';
+  }
+  const modalMessageFormatted = modalMessage.split('\n').map((line, index) => (
+    <span key={index}>
+      {line}
+      <br />
+    </span>
+  ));
+
   return (
-    <>
-      <S.Overlay onClick={handleConfirmClick}></S.Overlay>
-      <S.Modal>
-        <>
+    <S.Overlay>
+      {modalMessage === '' ? (
+        // 모든 조건이 충족되었을 때
+        <S.Modal>
           <S.CloseBtn src={Close} onClick={() => setOpenPostModal(false)} />
-          <S.ModalText>핫한🥵 썰 게시 완료</S.ModalText>
+          <S.ModalText>핫한🥵 썰 게시 완료!</S.ModalText>
           <S.ButtonDiv>
             <S.CancelButton onClick={handleViewPostClick}>
               글 보러 가기
             </S.CancelButton>
             <S.ConfirmButton onClick={handleConfirmClick}>확인</S.ConfirmButton>
           </S.ButtonDiv>
-        </>
-        {/* <>
+        </S.Modal>
+      ) : (
+        <S.Modal>
           <S.CloseBtn src={Close} onClick={() => setOpenPostModal(false)} />
-          <S.ModalText>
-            아차차,,
-            <br />
-            제목을 정해주셔야 게시를 할 수 있어요!
-          </S.ModalText>
+          <S.ModalText>{modalMessageFormatted}</S.ModalText>
           <S.ButtonDiv>
-            <S.ConfirmButton onClick={handleConfirmClick}>확인</S.ConfirmButton>
+            <S.ConfirmButton onClick={handleOkayClick}>확인</S.ConfirmButton>
           </S.ButtonDiv>
-        </>
-        <>
-          <S.CloseBtn src={Close} onClick={() => setOpenPostModal(false)} />
-          <S.ModalText>
-            아차차,,
-            <br />
-            내용을 적어주셔야 게시를 할 수 있어요!
-          </S.ModalText>
-          <S.ButtonDiv>
-            <S.ConfirmButton onClick={handleConfirmClick}>확인</S.ConfirmButton>
-          </S.ButtonDiv>
-        </>
-        <>
-          <S.CloseBtn src={Close} onClick={() => setOpenPostModal(false)} />
-          <S.ModalText>
-            아차차,,
-            <br />
-            대표 짤을 지정해주셔야 게시를 할 수 있어요!
-          </S.ModalText>
-          <S.ButtonDiv>
-            <S.ConfirmButton onClick={handleConfirmClick}>확인</S.ConfirmButton>
-          </S.ButtonDiv>
-        </>
-        <>
-          <S.CloseBtn src={Close} onClick={() => setOpenPostModal(false)} />
-          <S.ModalText>
-            아차차,,
-            <br />
-            키워드는 꼭 3개를 지정해주셔야 저장이 가능해요!
-          </S.ModalText>
-          <S.ButtonDiv>
-            <S.ConfirmButton onClick={handleConfirmClick}>확인</S.ConfirmButton>
-          </S.ButtonDiv>
-        </> */}
-      </S.Modal>
-    </>
+        </S.Modal>
+      )}
+    </S.Overlay>
   );
 };
 
