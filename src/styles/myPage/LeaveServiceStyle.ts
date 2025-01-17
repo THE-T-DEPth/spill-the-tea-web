49d6e59bd3f84f242bb3db3c29f6668058b550b5
ksy132336 <ksygiru@gmail.com>
2025-01-17 14:25:59 +0900

    feat: 서비스 떠나기 ui

--- src/styles/myPage/LeaveServiceStyle.ts
+++ src/styles/myPage/LeaveServiceStyle.ts
@@ -1,14 +1,24 @@
 import styled from 'styled-components';
+import { isMobile } from '../../hooks/Media';
 
 export const Container = styled.div`
   width: 556px;
   height: 227px;
   margin: 210px auto 332px;
   position: relative;
+
+  ${isMobile} {
+    width: 264px;
+  }
 `;
 
 export const Title = styled.p`
   font: var(--headingXXL);
+  text-align: center;
+
+  ${isMobile} {
+    font: var(--labelMedium);
+  }
 `;
 
 export const ButtonContainer = styled.div`
@@ -17,6 +27,13 @@ export const ButtonContainer = styled.div`
   display: flex;
   gap: 116px;
   margin: 124px 49px 0;
+
+  ${isMobile} {
+    width: 100%;
+    height: 33px;
+    gap: 48px;
+    margin: 61px 0 0;
+  }
 `;
 
 export const LogoutButton = styled.button`
@@ -30,6 +47,12 @@ export const LogoutButton = styled.button`
   border-radius: 4px;
   text-align: center;
   cursor: pointer;
+
+  ${isMobile} {
+    width: 108px;
+    height: 33px;
+    font: var(--changeButtonMini);
+  }
 `;
 
 export const DeleteAccountButton = styled.button`
@@ -43,4 +66,10 @@ export const DeleteAccountButton = styled.button`
   border-radius: 4px;
   text-align: center;
   cursor: pointer;
+
+  ${isMobile} {
+    width: 108px;
+    height: 33px;
+    font: var(--changeButtonMini);
+  }
 `;
