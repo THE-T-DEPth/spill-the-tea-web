4d86d840ae1e815156dc9aa28fad0ada3ef995be
ksy132336 <ksygiru@gmail.com>
2025-01-22 05:42:45 +0900

    feat: 내가 좋아요 누른 게시글 페이지 api 연동

--- src/pages/LikedSsulPage.tsx
+++ src/pages/LikedSsulPage.tsx
@@ -1,38 +1,72 @@
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from '../styles/likedssuls/LikedSsulPageStyle';
 import TopBar from '../components/searchResult/TopBar';
 import SortButton from '../components/likedssuls/SortButton';
 import Box from '../components/searchResult/Box';
-import BoxData from '../assets/data/BoxData';
 import { BoxProps } from '../components/searchResult/Box';
 import Pagination from '../components/searchResult/Pagination';
+import { getMyLikedPosts } from '../api/likedSsuls/getMyLIkedPosts';
 
 const LikedSsulPage = () => {
-  const [currentItems, setCurrentItems] = useState(BoxData.slice(0, 15)); // 초기 데이터
+  const [posts, setPosts] = useState<BoxProps[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortBy, setSortBy] = useState('DATE_DESC');
+  const [totalPages, setTotalPages] = useState(1);
 
-  const handlePageChange = useCallback((pageItems: BoxProps[]) => {
-    setCurrentItems(pageItems);
-  }, []);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await getMyLikedPosts(currentPage - 1, 15, sortBy);
+        if (response && response.success) {
+          const formattedPosts = response.data.contents.map((post) => ({
+            postId: post.postId,
+            title: post.title,
+            image: post.thumb,
+            keywords: post.keywordList
+              .replace(/\[|\]/g, '')
+              .split(', ')
+              .map((keyword) => `# ${keyword.trim()}`),
+            date: post.createdDateTime,
+            likes: post.likedCount,
+            comments: post.commentCount,
+          }));
+          setPosts(formattedPosts);
+          setTotalPages(response.data.totalPage);
+        }
+      } catch (error) {
+        console.error(
+          '좋아요 한 게시글 데이터를 불러오는 중 오류 발생:',
+          error
+        );
+      }
+    };
+    fetchPosts();
+  }, [currentPage, sortBy]);
+
+  const handleSortChange = (selectedSort: string) => {
+    setSortBy(selectedSort);
+    setCurrentPage(1);
+  };
 
   return (
     <>
       <TopBar text='"내가 공감을 누른 티 컬렉션🍵"' />
-      {BoxData.length === 0 ? (
+      {posts.length === 0 ? (
         <S.EmptyContainer>
           <S.EmptyMessage>아직은 공감을 누른 티가 없어요!</S.EmptyMessage>
         </S.EmptyContainer>
       ) : (
         <S.Container>
-          <SortButton />
+          <SortButton pageType='likedSsuls' onSortChange={handleSortChange} />
           <S.GridContainer>
-            {currentItems.map((data, index) => (
+            {posts.map((data) => (
               <Box
-                key={index}
+                key={data.postId}
+                postId={data.postId}
                 title={data.title}
                 image={data.image}
                 keywords={data.keywords}
                 date={data.date}
-                time={data.time}
                 likes={data.likes}
                 comments={data.comments}
               />
@@ -40,10 +74,9 @@ const LikedSsulPage = () => {
           </S.GridContainer>
           <S.PaginationContainer>
             <Pagination
-              totalItems={BoxData.length}
-              itemsPerPage={15}
-              items={BoxData}
-              onPageChange={handlePageChange}
+              totalPages={totalPages}
+              currentPage={currentPage}
+              onPageChange={setCurrentPage}
             />
           </S.PaginationContainer>
         </S.Container>
