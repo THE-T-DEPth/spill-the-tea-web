7351442258d328a351445854d69a7ca32bac2951
hayoung Park <mcd158@naver.com>
2025-01-23 11:45:08 +0900

    fix : 충돌 수정

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -5,15 +5,6 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { getcheckNicknameAvailability, postRegisterUser } from '../api/signUp/signUpPage';
 import useNSMediaQuery from "../hooks/useNSMediaQuery";
 
-// Dummy API for nickname check
-const checkNicknameAvailability = (nickname: string): Promise<'valid' | 'invalid'> => {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(nickname === 'existingNickname' ? 'invalid' : 'valid');
-		}, 500);
-	});
-};
-
 const SignupPage: React.FC = () => {
 
 	const location = useLocation();
